2fa67024fe48ab854a87ea343f491e59e935c7bc
Beckham624 <elvisbeckham624@gmail.com>
2026-03-24 21:02:23 +0300

    Add files via upload

--- ex1_mobile_money.c
+++ ex1_mobile_money.c
@@ -1,11 +1,22 @@
-/* Name: Your Full Name */
-/* Student Number: Your Registration Number */
-
+/*Name: OKELLO ELVIS BECHKAM */
+/*Student Number: 25/U/BIE/01412/PE */
 #include <stdio.h>
-
 int main(void)
 {
-    /* Write your code here */
-
+    float amount, percentage, fee, total;
+    //Ask user input
+    printf("Enter amount to send: ");
+    scanf("%f", &amount);
+    printf("Enter transaction fee (%%): ");
+    scanf("%f", &percentage);
+    //Calculate fee
+    fee = (percentage / 100) * amount;
+    //Calculate total deducted
+    total = amount + fee;
+    //Display results
+    printf("\n-----TRANSACTION SUMMARY-----\n");
+    printf("Amount        : %.2fUGX\n", amount);
+    printf("\nTransaction fee: %.2fUGX\n", fee);
+    printf("Total deducted: %.2fUGX\n", total);
     return 0;
 }
